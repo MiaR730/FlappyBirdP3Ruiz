f88de52d14ee15bf33678be5fda25ef82b99441d
MiaR730 <723420@student.dusd.net>
2023-12-12 11:46:07 -0800

    Added score and columns.

--- FlappyBirdRuizp3/Assets/Scripts/GameControl.cs
+++ FlappyBirdRuizp3/Assets/Scripts/GameControl.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
+using TMPro;
 public class GameControl : MonoBehaviour
 {
     public static GameControl instance;
     public GameObject gameOverText;
+    public TextMeshProUGUI scoretext;
     public bool gameOver = false;
     public float scrollspeed = -1.5f;
+
+    private int score = 0;
+
 
     // Start is called before the first frame update
     void Awake()
@@ -31,6 +36,17 @@ public class GameControl : MonoBehaviour
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
+
+    public void BirdScored()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+        score++;
+        scoretext.text = "Score: " + score.ToString();
+    }
+
 
     public void BirdDied()
     {
